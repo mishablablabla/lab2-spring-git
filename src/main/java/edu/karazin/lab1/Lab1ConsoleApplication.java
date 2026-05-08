3024a022ec1ba0e69f872d427c8703e76614306a
mishablablabla <mzuew2006@gmail.com>
2026-05-09 00:17:17 +0300

    feature branch change

--- src/main/java/edu/karazin/lab1/Lab1ConsoleApplication.java
+++ src/main/java/edu/karazin/lab1/Lab1ConsoleApplication.java
@@ -17,11 +17,7 @@ public class Lab1ConsoleApplication {
 		return args -> {
 			int value = 6;
 			long result = factorialService.factorial(value);
-<<<<<<< HEAD
 			System.out.println("Hello from feature branch");
-=======
-			System.out.println("Version from MAIN branch");
->>>>>>> 3a9c37b (main branch change)
 			System.out.println("Factorial(" + value + ") = " + result);
 		};
 	}
